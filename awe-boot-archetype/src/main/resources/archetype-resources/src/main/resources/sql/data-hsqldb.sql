113ced8799d1d548fcedd9c6727b7b690a6f6eae
ALMIS\pgarcia <pablo.garcia@almis.com>
2019-01-24 15:52:00 +0100

    Some fixes in archetype - Fixed database in archetype - Fixed java version in archetype

--- awe-boot-archetype/src/main/resources/archetype-resources/src/main/resources/sql/data-hsqldb.sql
+++ awe-boot-archetype/src/main/resources/archetype-resources/src/main/resources/sql/data-hsqldb.sql
@@ -3,19 +3,19 @@
 --  DDL for INSERT DATA
 --------------------------------------------------------
 -- Insert sequences
-Insert into AweKey (KeyNam, KeyVal) values ('OpeKey', 0);
-Insert into AweKey (KeyNam, KeyVal) values ('ThmKey', 0);
-Insert into AweKey (KeyNam, KeyVal) values ('ProKey', 0);
-Insert into AweKey (KeyNam, KeyVal) values ('ModKey', 0);
+Insert into AweKey (KeyNam, KeyVal) values ('OpeKey', 2);
+Insert into AweKey (KeyNam, KeyVal) values ('ThmKey', 19);
+Insert into AweKey (KeyNam, KeyVal) values ('ProKey', 5);
+Insert into AweKey (KeyNam, KeyVal) values ('ModKey', 3);
 Insert into AweKey (KeyNam, KeyVal) values ('DbsKey', 0);
 Insert into AweKey (KeyNam, KeyVal) values ('SitKey', 0);
 Insert into AweKey (KeyNam, KeyVal) values ('ModOpeKey', 0);
-Insert into AweKey (KeyNam, KeyVal) values ('ModProKey', 0);
+Insert into AweKey (KeyNam, KeyVal) values ('ModProKey', 5);
 Insert into AweKey (KeyNam, KeyVal) values ('SitModDbsKey', 0);
 Insert into AweKey (KeyNam, KeyVal) values ('ScrOpeKey', 0);
 Insert into AweKey (KeyNam, KeyVal) values ('ScrProKey', 0);
 Insert into AweKey (KeyNam, KeyVal) values ('EmlSrvKey', 0);
-Insert into AweKey (KeyNam, KeyVal) values ('AppParKey', 0);
+Insert into AweKey (KeyNam, KeyVal) values ('AppParKey', 20);
 Insert into AweKey (KeyNam, KeyVal) values ('JmsKey', 0);
 Insert into AweKey (KeyNam, KeyVal) values ('ScrCnfKey', 0);
 Insert into AweKey (KeyNam, KeyVal) values ('ScrResKey', 0);
@@ -39,8 +39,6 @@ Insert into AweThm (IdeThm, Nam, Act) values ('15','asphalt','1');
 Insert into AweThm (IdeThm, Nam, Act) values ('16','orange','1');
 Insert into AweThm (IdeThm, Nam, Act) values ('17','sea','1');
 Insert into AweThm (IdeThm, Nam, Act) values ('18','ferrari','1');
--- Update ThmKey
-UPDATE AweKey SET KeyVal = '19' where KeyNam = 'ThmKey';
 
 -- Insert Awe parameters
 Insert into AweAppPar (IdeAweAppPar,ParNam,ParVal,Cat,Des,Act) values ('1','DjrRepHisPth','/tmp/','2','Reports historize directory','1');
@@ -62,21 +60,32 @@ Insert into AweAppPar (IdeAweAppPar,ParNam,ParVal,Cat,Des,Act) values ('16','Max
 Insert into AweAppPar (IdeAweAppPar,ParNam,ParVal,Cat,Des,Act) values ('17','MaxFntVer','10','2','Maximum font size for vertical alignment','1');
 Insert into AweAppPar (IdeAweAppPar,ParNam,ParVal,Cat,Des,Act) values ('18','DjrHdgPag','0','2','Remove heading and pagination when exorting to excel','1');
 Insert into AweAppPar (IdeAweAppPar,ParNam,ParVal,Cat,Des,Act) values ('19','DjrBokTit','0','2','Include book in report title','1');
--- Update AppParKey
-UPDATE AweKey SET KeyVal = '20' where KeyNam = 'AppParKey';
 
 -- Insert default profiles
 Insert into AwePro (IdePro, Acr, Nam, IdeThm, ScrIni, Res, Act) values ('1','ADM','administrator','1',null,'administrator','1');
 Insert into AwePro (IdePro, Acr, Nam, IdeThm, ScrIni, Res, Act) values ('2','GNR','general','1',null,'general','1');
 Insert into AwePro (IdePro, Acr, Nam, IdeThm, ScrIni, Res, Act) values ('3','OPE','operator','1',null,'operator','1');
 Insert into AwePro (IdePro, Acr, Nam, IdeThm, ScrIni, Res, Act) values ('4','TST','test','1',null,'test','1');
--- Update ProKey
-UPDATE AweKey SET KeyVal = '5' where KeyNam = 'ProKey';
 
 -- Insert default user
-Insert into OPE (IdeOpe, l1_nom, l1_pas, l1_con, l1_dev, l1_act, l1_trt, l1_uti, l1_opr, l1_dat, imp_nom, dat_mod, l1_psd, l1_lan, l1_sgn, PcPrn, EmlSrv, EmlAdr, OpeNam, IdePro, IdeThm) values ((select KeyVal from AweKey where KeyNam = 'OpeKey' ),'mgr','0dc654ed35551j105b5c782a8fbb838f12c6678e',0,null,1,null,0,null,null,'none',null,null,'ENG',1,null,null,'mgr@awe.com','Manager',(select IdePro from AwePro where Nam = 'administrator'),(select IdeThm from AweThm where Nam = 'sunset'));
--- Update OpeKey
-UPDATE AweKey SET KeyVal = '2' where KeyNam = 'OpeKey';
+Insert into OPE (IdeOpe, l1_nom, l1_pas, l1_con, l1_dev, l1_act, l1_trt, l1_uti, l1_opr, l1_dat, imp_nom, dat_mod, l1_psd, l1_lan, l1_sgn, PcPrn, EmlSrv, EmlAdr, OpeNam, IdePro, IdeThm) values (1,'test','5e52fee47e6b070565f74372468cdc699de89107',0,null,1,null,0,null,null,'none',null,null,'ENG',1,null,null,'test@test.com','Manager',(select IdePro from AwePro where Nam = 'administrator'),(select IdeThm from AweThm where Nam = 'sunset'));
 
+-- Insert AweMod
+delete AweMod;
+insert into AweMod (IdeMod, Nam, ScrIni, IdeThm, Act) values (1,	'Base',	'Sit',	2,	1);
+insert into AweMod (IdeMod, Nam, ScrIni, IdeThm, Act) values (2,	'Test',	'Dbs',	4,	1);
+
+-- Insert AweMod
+delete AweDbs;
+
+-- Insert AweSitModDbs
+delete AweSitModDbs;
+
+-- Insert AweModPro
+delete AweModPro;
+insert into AweModPro (IdeModPro, IdeMod, IdePro, Ord) values (1,	2,	1,	null);
+insert into AweModPro (IdeModPro, IdeMod, IdePro, Ord) values (2,	1,	1,	null);
+insert into AweModPro (IdeModPro, IdeMod, IdePro, Ord) values (3,	1,	2,	null);
+insert into AweModPro (IdeModPro, IdeMod, IdePro, Ord) values (4,	1,	3,	null);
 
 
