78d85deacdb558f2854855b4d339cc5a670f6237
ALMIS\pgarcia <pablo.garcia@almis.com>
2019-01-28 14:13:10 +0100

    Fixed FileManager - Include in awe-boot - Sonar issues

--- awe-controller/src/main/java/com/almis/awe/controller/InitController.java
+++ awe-controller/src/main/java/com/almis/awe/controller/InitController.java
@@ -6,7 +6,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
@@ -38,7 +38,7 @@ public class InitController {
    * Handler for index page
    * @return Index page
    */
-  @RequestMapping (value = {"/", "/screen/**"})
+  @GetMapping(value = {"/", "/screen/**"})
   public String index() {
     return "index";
   }
