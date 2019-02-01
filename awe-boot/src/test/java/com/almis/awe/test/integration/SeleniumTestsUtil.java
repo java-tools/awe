ea811f91f28ffbb96aa1a82e712df5f394aee5b3
ALMIS\pgarcia <pablo.garcia@almis.com>
2019-02-01 17:44:07 +0100

    Updated selenium tests

--- awe-boot/src/test/java/com/almis/awe/test/integration/SeleniumTestsUtil.java
+++ awe-boot/src/test/java/com/almis/awe/test/integration/SeleniumTestsUtil.java
@@ -7,12 +7,13 @@ import org.junit.AfterClass;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-//import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.opera.OperaDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
@@ -35,12 +36,12 @@ public class SeleniumTestsUtil {
   private static WebDriver driver;
 
   @Value("${selenium.start.url}")
-  String startURL = "http://localhost:8080/awe-boot";
+  String startURL;
 
-  @Value("${selenium.timeout:30}")
-  Integer timeout = 30;
+  @Value("${failsafe.timeout:30}")
+  Integer timeout;
 
-  @Value("${selenium.browser:chrome}")
+  @Value("${failsafe.browser:chrome}")
   public void setBrowser(String browser) {
     if (driver == null) {
       switch (browser) {
@@ -50,9 +51,9 @@ public class SeleniumTestsUtil {
           break;
         case "headless-firefox":
           WebDriverManager.firefoxdriver().setup();
-          /*FirefoxOptions firefoxOptions = new FirefoxOptions();
+          FirefoxOptions firefoxOptions = new FirefoxOptions();
           firefoxOptions.addArguments("--headless");
-          driver = new FirefoxDriver(firefoxOptions);*/
+          driver = new FirefoxDriver(firefoxOptions);
           break;
         case "headless-chrome":
           WebDriverManager.chromedriver().setup();
@@ -117,12 +118,215 @@ public class SeleniumTestsUtil {
     assertEquals(text, driver.findElement(selector).getText());
   }
 
-  protected void sendKeys(By selector, String text) {
+  protected void checkTextContains(By selector, String text) {
+    assertTrue(driver.findElement(selector).getText().contains(text));
+  }
+
+  protected void checkCriterionContains(By selector, String text) {
+    assertTrue(driver.findElement(selector).getAttribute("value").contains(text));
+  }
+
+  protected void sendKeys(By selector, CharSequence... text) {
     driver.findElement(selector).sendKeys(text);
+  }
+
+  protected void clearText(By selector) {
+    driver.findElement(selector).clear();
   }
 
   protected void click(By selector) {
     driver.findElement(selector).click();
+  }
+
+  protected void gotoScreen(String... menuOptions) {
+    for (String option : menuOptions) {
+      // Wait for text in selector
+      waitUntil(presenceOfElementLocated(By.name(option)));
+
+      // Click on screen
+      click(By.name(option));
+    }
+  }
+
+  protected void waitForLoadingBar() {
+    // Wait for element not visible
+    waitUntil(invisibilityOfElementLocated(By.id("loading-bar")));  }
+
+  protected void waitForLoadingGrid() {
+    By selector = By.cssSelector(".grid-loader");
+
+    // Wait for element visible
+    waitUntil(visibilityOfElementLocated(selector));
+
+    // Wait for element not visible
+    waitUntil(invisibilityOfElementLocated(selector));
+  }
+
+  protected void clickButton(String buttonName) {
+    // Wait for element visible
+    waitForButton(buttonName);
+
+    // Click button
+    click(By.cssSelector("#" + buttonName + ":not([disabled])"));
+  }
+
+  protected void clickRowContents(String search) {
+    By selector = By.xpath("//*[contains(@class,'ui-grid-row')]//*[contains(@class,'ui-grid-cell-contents')]//text()[contains(.,'" + search +"')]/..");
+
+    // Wait for element visible
+    waitUntil(visibilityOfElementLocated(selector));
+
+    // Click button
+    click(selector);
+  }
+
+  protected void checkRowContents(String search) {
+    By selector = By.xpath("//*[contains(@class,'ui-grid-row')]//*[contains(@class,'ui-grid-cell-contents')]//text()[contains(.,'" + search +"')]/..");
+
+    // Wait for element visible
+    waitUntil(visibilityOfElementLocated(selector));
+
+    // Check text
+    checkTextContains(selector, search);
+  }
+
+  protected void checkCriterionContents(String criterionName, String search) {
+    By selector = By.cssSelector("[criterion-id='" + criterionName +  "'] input");
+
+    // Wait for element visible
+    waitUntil(visibilityOfElementLocated(selector));
+
+    // Check text
+    checkCriterionContains(selector, search);
+  }
+
+  protected void checkSelectorContents(String criterionName, String search) {
+    By selector = By.cssSelector("[criterion-id='" + criterionName +  "'] .select2-chosen");
+
+    // Wait for element visible
+    waitUntil(visibilityOfElementLocated(selector));
+
+    // Check text
+    checkTextContains(selector, search);
+  }
+
+  protected void waitForButton(String buttonName) {
+    // Wait for element visible
+    waitUntil(visibilityOfElementLocated(By.cssSelector("#" + buttonName + ":not([disabled])")));
+  }
+
+  protected void writeText(String criterionName, String text, boolean isColumn) {
+    String mainSelector = isColumn ? "column-id" : "criterion-id";
+    By selector = By.cssSelector("[" + mainSelector + "='" + criterionName +  "'] input");
+
+    // Wait for element present
+    waitUntil(presenceOfElementLocated(selector));
+
+    // Clear previous text
+    clearText(selector);
+
+    // Write text
+    sendKeys(selector, text);
+  }
+
+  private void selectClick(String criterionName, boolean isColumn) {
+    String mainSelector = isColumn ? "column-id" : "criterion-id";
+    By selector = By.cssSelector("[" + mainSelector + "='" + criterionName + "'] .select2-choice");
+
+    // Wait for element present
+    waitUntil(presenceOfElementLocated(selector));
+
+    // Click selector
+    click(selector);
+
+    // Wait for element present
+    waitUntil(presenceOfElementLocated(By.cssSelector("#select2-drop")));
+  }
+
+  protected void selectFirst(String criterionName, boolean isColumn) {
+    // Click on selector
+    selectClick(criterionName, isColumn);
+
+    // Click option
+    click(By.cssSelector("#select2-drop li:first-of-type"));
+  }
+
+  protected void selectLast(String criterionName, boolean isColumn) {
+    // Click on selector
+    selectClick(criterionName, isColumn);
+
+    // Click option
+    click(By.cssSelector("#select2-drop li:last-of-type"));
+  }
+
+  protected void selectContain(String criterionName, String label, boolean isColumn) {
+    By selector = By.xpath("//*[@id='select2-drop']//*[contains(@class,'select2-result-label')]//text()[contains(.,'" + label +"')]/..");
+
+    // Click on selector
+    selectClick(criterionName, isColumn);
+
+    // Wait for element present
+    waitUntil(presenceOfElementLocated(selector));
+
+    // Click option
+    click(selector);
+  }
+
+  protected void suggest(String criterionName, String search, String label, boolean isColumn) {
+    By selector = By.xpath("//*[@id='select2-drop']//*[contains(@class,'select2-result-label')]//text()[contains(.,'" + label +"')]/..");
+
+    // Click on selector
+    selectClick(criterionName, isColumn);
+
+    // Wait for element present
+    waitUntil(presenceOfElementLocated(By.cssSelector("#select2-drop input.select2-input")));
+
+    // Write username
+    sendKeys(By.cssSelector("#select2-drop input.select2-input"), search);
+
+    // Wait for element present
+    waitUntil(presenceOfElementLocated(selector));
+
+    // Click option
+    click(selector);
+  }
+
+  protected void saveLine() {
+    saveLine(null);
+  }
+
+  protected void saveLine(String gridId) {
+    String gridSelector = gridId == null ? "" : "[grid-id='" + gridId + "'] ";
+    By selector = By.cssSelector(gridSelector + ".grid-row-save:not([disabled])");
+
+    // Wait for element present
+    waitUntil(presenceOfElementLocated(selector));
+
+    // Click option
+    click(selector);
+
+    // Wait for element present
+    waitUntil(presenceOfElementLocated(selector));
+  }
+
+  protected void acceptConfirm() {
+    clickButton("confirm-accept");
+
+    // Wait for element not present
+    waitUntil(invisibilityOfElementLocated(By.id("confirm-accept")));
+  }
+
+  protected void acceptMessage(String messageType) {
+    By messageSelector = By.cssSelector(".alert-zone .alert-" + messageType + " button.close");
+
+    // Wait for message selector
+    waitUntil(presenceOfElementLocated(messageSelector));
+
+    // Click on message selector
+    click(messageSelector);
+
+    // Wait for element not present
+    waitUntil(invisibilityOfElementLocated(messageSelector));
   }
 
   protected void doLogin() throws Exception {
@@ -138,25 +342,19 @@ public class SeleniumTestsUtil {
     waitUntil(textMatches(By.cssSelector("div.slogan"), Pattern.compile("Almis Web Engine")));
 
     // Wait for element present
-    waitUntil(presenceOfElementLocated(By.cssSelector("#ButLogIn:not([disabled])")));
-
-    // Wait for element present
-    waitUntil(presenceOfElementLocated(By.cssSelector("[criterion-id='cod_usr']")));
+    waitForButton("ButLogIn");
 
     // Write username
-    sendKeys(By.cssSelector("[criterion-id='cod_usr'] input"), "test");
+    writeText("cod_usr", "test", false);
 
     // Write password
-    sendKeys(By.cssSelector("[criterion-id='pwd_usr'] input"), "test");
-
-    // Wait for element visible
-    waitUntil(visibilityOfElementLocated(By.cssSelector("#ButLogIn:not([disabled])")));
+    writeText("pwd_usr", "test", false);
 
     // Click button
-    click(By.cssSelector("#ButLogIn:not([disabled])"));
+    clickButton("ButLogIn");
 
     // Wait for element not visible
-    waitUntil(invisibilityOfElementLocated(By.id("loading-bar")));
+    waitForLoadingBar();
 
     // Wait for element present
     waitUntil(presenceOfElementLocated(By.cssSelector("#ButUsrAct span.info-text")));
@@ -166,5 +364,25 @@ public class SeleniumTestsUtil {
 
     // Assertion
     checkText(By.cssSelector("#ButUsrAct span.info-text"), "Manager (test)");
+  }
+
+  protected void doLogout() throws Exception {
+    // Wait for element not visible
+    waitForLoadingBar();
+
+    // Wait for element present
+    waitUntil(presenceOfElementLocated(By.cssSelector("#ButUsrAct span.info-text")));
+
+    // Wait for element present
+    waitUntil(textMatches(By.cssSelector("#ButUsrAct span.info-text"), Pattern.compile("Manager \\(test\\)")));
+
+    // Wait for element present
+    clickButton("ButLogOut");
+
+    // Wait for element not visible
+    waitForLoadingBar();
+
+    // Wait for text in selector
+    waitUntil(textMatches(By.cssSelector("div.slogan"), Pattern.compile("Almis Web Engine")));
   }
 }
--- awe-boot/src/test/java/com/almis/awe/test/integration/StaticTestsIT.java
+++ awe-boot/src/test/java/com/almis/awe/test/integration/StaticTestsIT.java
@@ -1,33 +1,506 @@
 package com.almis.awe.test.integration;
 
 
+import org.junit.FixMethodOrder;
 import org.junit.Test;
-import org.openqa.selenium.By;
+import org.junit.runners.MethodSorters;
 
-import static org.openqa.selenium.support.ui.ExpectedConditions.*;
-
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class StaticTestsIT extends SeleniumTestsUtil {
 
   @Test
-  public void loginTest() throws Exception {
+  public void t000_loginTest() throws Exception {
     doLogin();
   }
 
   @Test
-  public void newSite() throws Exception {
-    // Wait for text in selector
-    waitUntil(presenceOfElementLocated(By.name("tools")));
+  public void t999_logoutTest() throws Exception {
+    doLogout();
+  }
 
-    // Click on screen
-    click(By.name("tools"));
+  @Test
+  public void t001_newSite() throws Exception {
+    // Go to screen
+    gotoScreen("tools", "sites");
 
-    // Wait for text in selector
-    waitUntil(visibilityOfElementLocated(By.name("sites")));
+    // Wait for loading bar
+    waitForLoadingBar();
 
-    // Click on screen
-    click(By.name("sites"));
+    // Click on new button
+    clickButton("ButNew");
 
-    // Wait for element not visible
-    waitUntil(invisibilityOfElementLocated(By.id("loading-bar")));
+    // Wait for loading bar
+    waitForLoadingBar();
+
+    // Wait for button
+    waitForButton("ButCnf");
+
+    // Write on criterion
+    writeText("Nam", "Site test", false);
+
+    // Select on selector
+    selectLast("Act", false);
+
+    // Write on criterion
+    writeText("Ord", "3", false);
+
+    // Click on button
+    clickButton("ButGrdAdd");
+
+    // Suggest on column selector
+    suggest("IdeMod", "Base", "Base", true);
+
+    // Suggest on column selector
+    suggest("IdeDbs", "awedb", "awedb", true);
+
+    // Write on criterion
+    writeText("Order", "3", true);
+
+    // Save line
+    saveLine();
+
+    // Click on button
+    clickButton("ButCnf");
+
+    // Accept confirm
+    acceptConfirm();
+
+    // Accept message
+    acceptMessage("success");
+  }
+
+  @Test
+  public void t002_verifyNewSite() throws Exception {
+    // Go to screen
+    gotoScreen("tools", "sites");
+
+    // Wait for loading bar
+    waitForLoadingBar();
+
+    // Wait for button
+    clickButton("ButRst");
+
+    // Suggest on column selector
+    suggest("CrtSit", "Site test", "Site test", false);
+
+    // Click on button
+    clickButton("ButSch");
+
+    // Wait for loading bar
+    waitForLoadingGrid();
+
+    // Click row
+    clickRowContents("Site test");
+
+    // Click on button
+    clickButton("ButViw");
+
+    // Wait for loading bar
+    waitForLoadingBar();
+
+    // Wait for button
+    waitForButton("ButBck");
+
+    // Check row contents
+    checkRowContents("Base");
+
+    // Check row contents
+    checkRowContents("awedb");
+  }
+
+  @Test
+  public void t003_updateSite() throws Exception {
+    // Go to screen
+    gotoScreen("tools", "sites");
+
+    // Wait for loading bar
+    waitForLoadingBar();
+
+    // Wait for button
+    clickButton("ButRst");
+
+    // Suggest on column selector
+    suggest("CrtSit", "Site", "Site", false);
+
+    // Click on button
+    clickButton("ButSch");
+
+    // Wait for loading bar
+    waitForLoadingGrid();
+
+    // Click row
+    clickRowContents("Site");
+
+    // Click on button
+    clickButton("ButUpd");
+
+    // Wait for loading bar
+    waitForLoadingBar();
+
+    // Wait for button
+    waitForButton("ButCnf");
+
+    // Write on criterion
+    writeText("Nam", "Site changed", false);
+
+    // Click row
+    clickRowContents("Base");
+
+    // Suggest on column selector
+    suggest("IdeDbs", "awedb2", "awedb2", true);
+
+    // Save line
+    saveLine();
+
+    // Click on button
+    clickButton("ButCnf");
+
+    // Accept confirm
+    acceptConfirm();
+
+    // Accept message
+    acceptMessage("success");
+  }
+
+  @Test
+  public void t004_verifyUpdatedSite() throws Exception {
+    // Go to screen
+    gotoScreen("tools", "sites");
+
+    // Wait for loading bar
+    waitForLoadingBar();
+
+    // Wait for button
+    clickButton("ButRst");
+
+    // Suggest on column selector
+    suggest("CrtSit", "Site changed", "Site changed", false);
+
+    // Click on button
+    clickButton("ButSch");
+
+    // Wait for loading bar
+    waitForLoadingGrid();
+
+    // Click row
+    clickRowContents("Site changed");
+
+    // Click on button
+    clickButton("ButViw");
+
+    // Wait for loading bar
+    waitForLoadingBar();
+
+    // Wait for button
+    waitForButton("ButBck");
+
+    // Check row contents
+    checkRowContents("Base");
+
+    // Check row contents
+    checkRowContents("awedb2");
+  }
+
+  @Test
+  public void t011_newModule() throws Exception {
+    // Go to screen
+    gotoScreen("tools", "modules");
+
+    // Wait for loading bar
+    waitForLoadingBar();
+
+    // Click on new button
+    clickButton("ButNew");
+
+    // Wait for loading bar
+    waitForLoadingBar();
+
+    // Wait for button
+    waitForButton("ButCnf");
+
+    // Select option
+    suggest("Nam", "Inf", "Inf", false);
+
+    // Suggest on selector
+    suggest("Scr", "Sites", "Sites", false);
+
+    // Suggest on  selector
+    suggest("Thm", "gra", "gra", false);
+
+    // Select on selector
+    suggest("Act",  "Yes", "Yes", false);
+
+    // Click on button
+    clickButton("ButMdlUsrLstAdd");
+
+    // Suggest on column selector
+    suggest("IdeOpe", "test", "test", true);
+
+    // Suggest on column selector
+    suggest("IdeThm", "sky", "sky", true);
+
+    // Save line
+    saveLine("MdlUsrLst");
+
+    // Click on button
+    clickButton("ButMdlPrfLstAdd");
+
+    // Suggest on column selector
+    suggest("IdePro", "TS", "TS", true);
+
+    // Save line
+    saveLine("MdlPrfLst");
+
+    // Click on button
+    clickButton("ButMdlSitDbsLstAdd");
+
+    // Suggest on column selector
+    suggest("IdeSit", "Site", "Site", true);
+
+    // Suggest on column selector
+    suggest("IdeDbs", "awedb", "awedb", true);
+
+    // Save line
+    saveLine("MdlSitDbsLst");
+
+    // Click on button
+    clickButton("ButCnf");
+
+    // Accept confirm
+    acceptConfirm();
+
+    // Accept message
+    acceptMessage("success");
+  }
+
+  @Test
+  public void t012_verifyNewModule() throws Exception {
+    // Go to screen
+    gotoScreen("tools", "modules");
+
+    // Wait for loading bar
+    waitForLoadingBar();
+
+    // Wait for button
+    clickButton("ButRst");
+
+    // Suggest on column selector
+    suggest("CrtMod", "Inf", "Inf", false);
+
+    // Click on button
+    clickButton("ButSch");
+
+    // Wait for loading bar
+    waitForLoadingGrid();
+
+    // Click row
+    clickRowContents("Inf");
+
+    // Click on button
+    clickButton("ButViw");
+
+    // Wait for loading bar
+    waitForLoadingBar();
+
+    // Wait for button
+    waitForButton("ButBck");
+
+    // Check contents
+    checkCriterionContents("Nam", "Inf");
+
+    // Check contents
+    checkCriterionContents("Thm", "grass");
+
+    // Check contents
+    checkSelectorContents("Scr", "Sites (Sit)");
+
+    // Check row contents
+    checkRowContents("test");
+
+    // Check row contents
+    checkRowContents("TST");
+
+    // Check row contents
+    checkRowContents("Site changed");
+  }
+
+  @Test
+  public void t013_updateModule() throws Exception {
+    // Go to screen
+    gotoScreen("tools", "modules");
+
+    // Wait for loading bar
+    waitForLoadingBar();
+
+    // Wait for button
+    clickButton("ButRst");
+
+    // Suggest on column selector
+    suggest("CrtMod", "Inf", "Inf", false);
+
+    // Click on button
+    clickButton("ButSch");
+
+    // Wait for loading bar
+    waitForLoadingGrid();
+
+    // Click row
+    clickRowContents("Inf");
+
+    // Click on button
+    clickButton("ButUpd");
+
+    // Wait for loading bar
+    waitForLoadingBar();
+
+    // Wait for button
+    waitForButton("ButCnf");
+
+    // Write on criterion
+    suggest("Nam", "Inf changed","Inf changed", false);
+
+    // Write on criterion
+    suggest("Scr", "Usr", "Usr", false);
+
+    // Click row
+    clickRowContents("test");
+
+    // Suggest on column selector
+    selectContain("IdeThm", "grass", true);
+
+    // Save line
+    saveLine("MdlUsrLst");
+
+    // Click row
+    clickRowContents("TST");
+
+    // Suggest on column selector
+    selectContain("IdePro", "ADM - administrator", true);
+
+    // Save line
+    saveLine("MdlPrfLst");
+
+    // Click row
+    clickRowContents("Site changed");
+
+    // Suggest on column selector
+    selectContain("IdeDbs", "awedb", true);
+
+    // Save line
+    saveLine("MdlSitDbsLst");
+
+    // Click on button
+    clickButton("ButCnf");
+
+    // Accept confirm
+    acceptConfirm();
+
+    // Accept message
+    acceptMessage("success");
+  }
+
+  /*@Test
+  public void t014_verifyUpdatedModule() throws Exception {
+    // Go to screen
+    gotoScreen("tools", "modules");
+
+    // Wait for loading bar
+    waitForLoadingBar();
+
+    // Wait for button
+    clickButton("ButRst");
+
+    // Suggest on column selector
+    suggest("CrtSit", "Site changed", "Site changed", false);
+
+    // Click on button
+    clickButton("ButSch");
+
+    // Wait for loading bar
+    waitForLoadingGrid();
+
+    // Click row
+    clickRowContents("Site changed");
+
+    // Click on button
+    clickButton("ButViw");
+
+    // Wait for loading bar
+    waitForLoadingBar();
+
+    // Wait for button
+    waitForButton("ButBck");
+
+    // Check row contents
+    checkRowContents("Base");
+
+    // Check row contents
+    checkRowContents("awedb2");
+  }*/
+
+  @Test
+  public void t055_deleteModule() throws Exception {
+    // Go to screen
+    gotoScreen("tools", "modules");
+
+    // Wait for loading bar
+    waitForLoadingBar();
+
+    // Wait for button
+    clickButton("ButRst");
+
+    // Suggest on column selector
+    suggest("CrtMod", "Inf", "Inf", false);
+
+    // Click on button
+    clickButton("ButSch");
+
+    // Wait for loading bar
+    waitForLoadingGrid();
+
+    // Click row
+    clickRowContents("Inf");
+
+    // Click on button
+    clickButton("ButDel");
+
+    // Accept confirm
+    acceptConfirm();
+
+    // Accept message
+    acceptMessage("success");
+  }
+
+  @Test
+  public void t056_deleteSite() throws Exception {
+    // Go to screen
+    gotoScreen("tools", "sites");
+
+    // Wait for loading bar
+    waitForLoadingBar();
+
+    // Wait for button
+    clickButton("ButRst");
+
+    // Suggest on column selector
+    suggest("CrtSit", "Site", "Site", false);
+
+    // Click on button
+    clickButton("ButSch");
+
+    // Wait for loading bar
+    waitForLoadingGrid();
+
+    // Click row
+    clickRowContents("Site");
+
+    // Click on button
+    clickButton("ButDel");
+
+    // Accept confirm
+    acceptConfirm();
+
+    // Accept message
+    acceptMessage("success");
   }
 }
