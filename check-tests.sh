fc2e8256a18164c4d352897f1c0fcee4a9bb97fe
ALMIS\pgarcia <pablo.garcia@almis.com>
2019-02-11 14:42:02 +0100

    Changed the testing script to one line

--- check-tests.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-errors=`cat awe-boot/target/surefire-reports/TEST-com.almis.awe.test.AllTestsSuiteIT.xml | grep 'errors\=\"[^\"]*' | grep 'failures\=\"[^\"]*' | sed -e 's/.*errors="//g' | sed -e 's/\"\s*skipped="[^\"]*"\s*failures="/\t/g' | sed -e 's/">//g' | awk 'BEGIN { FPAT="[0-9]+"} {printf ($1+$2>0)}'`
-exit $errors
