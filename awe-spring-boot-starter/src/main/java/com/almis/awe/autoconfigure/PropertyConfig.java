59bea1c0068fa854c92d35d7da2f9c41bdb1e93b
ALMIS\pgarcia <pablo.garcia@almis.com>
2019-03-26 10:11:44 +0100

    Fixed sonar issues

--- awe-spring-boot-starter/src/main/java/com/almis/awe/autoconfigure/PropertyConfig.java
+++ awe-spring-boot-starter/src/main/java/com/almis/awe/autoconfigure/PropertyConfig.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.core.convert.ConversionService;
