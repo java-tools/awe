5737e5eecc083d36f79c7712d0c8558d42f277d9
pvidal <pablo.vidal@almis.com>
2019-11-18 17:17:17 +0100

    Fix problem with DIFF operations in Oracle database
    Signed-off-by: pvidal <pablo.vidal@almis.com>

--- awe-framework/awe-controller/src/main/java/com/almis/awe/template/FixedOracleTemplates.java
+++ awe-framework/awe-controller/src/main/java/com/almis/awe/template/FixedOracleTemplates.java
@@ -14,10 +14,11 @@ public class FixedOracleTemplates extends OracleTemplates {
   public FixedOracleTemplates() {
     super();
     // querydsl-sql library (v3.6.2) has a wrong datediff template for oracle, so we redefine it:
-    add(Ops.DateTimeOps.DIFF_WEEKS, "round(extract(day from ({1} - {0}) / 7))");
-    add(Ops.DateTimeOps.DIFF_DAYS, "extract(day from {1} - {0})");
-    add(Ops.DateTimeOps.DIFF_HOURS, "extract(day from ({1} - {0}) * 24)");
-    add(Ops.DateTimeOps.DIFF_MINUTES, "extract(day from ({1} - {0}) * 1440)");
-    add(Ops.DateTimeOps.DIFF_SECONDS, "extract(day from ({1} - {0}) * 86400)");
+    // https://github.com/querydsl/querydsl/pull/1590/commits/f9f54bb2bd4213dc9bb43bb8b5c2f00958fb77b0
+    add(Ops.DateTimeOps.DIFF_WEEKS, "round((cast({1} as date) - cast({0} as date)) / 7)");
+    add(Ops.DateTimeOps.DIFF_DAYS, "round(cast({1} as date) - cast({0} as date))");
+    add(Ops.DateTimeOps.DIFF_HOURS, "round((cast({1} as date) - cast({0} as date)) * 24)");
+    add(Ops.DateTimeOps.DIFF_MINUTES, "round((cast({1} as date) - cast({0} as date)) * 1440)");
+    add(Ops.DateTimeOps.DIFF_SECONDS, "round((cast({1} as date) - cast({0} as date)) * 86400)");
   }
 }
