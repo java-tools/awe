c90c76474154137d28a21a7cea946334fc0cd27c
Pablo Garcia <pablo.garcia@almis.com>
2019-12-04 08:44:09 +0100

    Added Lombok to FileData Added a QueryData object

--- awe-framework/awe-model/src/main/java/com/almis/awe/model/dto/FileData.java
+++ awe-framework/awe-model/src/main/java/com/almis/awe/model/dto/FileData.java
@@ -1,6 +1,9 @@
 package com.almis.awe.model.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
 
 import java.io.InputStream;
 import java.io.Serializable;
@@ -10,10 +13,11 @@ import java.io.Serializable;
  *
  * @author pgarcia
  */
+@Data
+@NoArgsConstructor
+@Accessors(chain = true)
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public final class FileData implements Serializable {
-
-  private static final long serialVersionUID = 7037606445027020703L;
 
   private String fileName;
   private Long fileSize;
@@ -21,12 +25,6 @@ public final class FileData implements Serializable {
   private String basePath;
   private String relativePath;
   private transient InputStream fileStream;
-
-  /**
-   * Empty constructor
-   */
-  public FileData() {
-  }
 
   /**
    * Copy constructor
@@ -68,117 +66,5 @@ public final class FileData implements Serializable {
     this.fileSize = fileSize;
     this.mimeType = mimeType;
     this.relativePath = relativePath == null || relativePath.isEmpty() ? null : relativePath;
-  }
-
-  /**
-   * Retrieve the file name
-   *
-   * @return the fileName
-   */
-  public String getFileName() {
-    return fileName;
-  }
-
-  /**
-   * Store the file name
-   *
-   * @param fileName the fileName to set
-   */
-  public void setFileName(String fileName) {
-    this.fileName = fileName;
-  }
-
-  /**
-   * Retrieve the file size
-   *
-   * @return the fileSize
-   */
-  public Long getFileSize() {
-    return fileSize;
-  }
-
-  /**
-   * Store the file size
-   *
-   * @param fileSize the fileSize to set
-   */
-  public void setFileSize(Long fileSize) {
-    this.fileSize = fileSize;
-  }
-
-  /**
-   * Retrieve the file mime type
-   *
-   * @return the contentType
-   */
-  public String getMimeType() {
-    return mimeType;
-  }
-
-  /**
-   * Store the file mime type
-   *
-   * @param contentType the contentType to set
-   */
-  public void setMimeType(String contentType) {
-    this.mimeType = contentType;
-  }
-
-  /**
-   * Retrieve the file stream if defined
-   *
-   * @return the fileStream
-   */
-  public InputStream getFileStream() {
-    return fileStream;
-  }
-
-  /**
-   * Store the file stream
-   *
-   * @param fileStream the fileStream to set
-   */
-  public void setFileStream(InputStream fileStream) {
-    this.fileStream = fileStream;
-  }
-
-  /**
-   * Get base path
-   *
-   * @return Base path
-   */
-  public String getBasePath() {
-    return basePath;
-  }
-
-  /**
-   * Set base path
-   *
-   * @param basePath base path
-   * @return file data
-   */
-  public FileData setBasePath(String basePath) {
-    this.basePath = basePath;
-    return this;
-  }
-
-  /**
-   * Get relative path
-   *
-   * @return Relative path
-   */
-  public String getRelativePath() {
-    return relativePath;
-  }
-
-  /**
-   * Set relative path
-   *
-   * @param relativePath Relative path
-   * @return file data
-   */
-  public FileData setRelativePath(String relativePath) {
-    this.relativePath = relativePath;
-    return this;
   }
 }
