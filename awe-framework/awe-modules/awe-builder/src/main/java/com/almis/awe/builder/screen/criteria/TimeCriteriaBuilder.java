c1db5c6fac6e78a95e512cdd717c2f9a888c6dac
ALMIS\pgarcia <pablo.garcia@almis.com>
2020-01-13 17:53:22 +0100

    Removed some sonar issues

--- awe-framework/awe-modules/awe-builder/src/main/java/com/almis/awe/builder/screen/criteria/TimeCriteriaBuilder.java
+++ awe-framework/awe-modules/awe-builder/src/main/java/com/almis/awe/builder/screen/criteria/TimeCriteriaBuilder.java
@@ -1,7 +1,6 @@
 package com.almis.awe.builder.screen.criteria;
 
 import com.almis.awe.builder.enumerates.Component;
-import com.almis.awe.builder.screen.base.AbstractComponentBuilder;
 import com.almis.awe.builder.screen.base.AbstractCriteriaBuilder;
 import com.almis.awe.model.entities.screen.component.criteria.AbstractCriteria;
 import com.almis.awe.model.entities.screen.component.criteria.Criteria;
