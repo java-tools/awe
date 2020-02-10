c4e13b72a91eaed59bbeda563ade230fe5d1ba2e
ALMIS\pgarcia <pablo.garcia@almis.com>
2020-02-10 16:43:18 +0100

    Changed Webpack to version 4

--- awe-framework/awe-client-angular/src/main/resources/webpack/locals-en.config.js
+++ awe-framework/awe-client-angular/src/main/resources/webpack/locals-en.config.js
@@ -1,4 +1,5 @@
 import "moment/locale/en-gb";
+import "numeral/locales/en-gb";
+import "./../js/lib/highcharts/i18n/highcharts.en";
 import "bootstrap-datepicker/js/locales/bootstrap-datepicker.en-GB";
 import "./../js/lib/pivotTable/i18n/pivot.en";
-import "./../js/lib/highcharts/i18n/highcharts.en";
--- awe-framework/awe-client-angular/src/main/resources/webpack/locals-fr.config.js
+++ awe-framework/awe-client-angular/src/main/resources/webpack/locals-fr.config.js
@@ -1,6 +1,6 @@
 import "moment/locale/fr";
 import "numeral/locales/fr";
-import "bootstrap-datepicker/js/locales/bootstrap-datepicker.fr";
-import "./../js/lib/pivotTable/i18n/pivot.fr";
 import "./../js/lib/highcharts/i18n/highcharts.fr";
-import "bootstrap-markdown/locale/bootstrap-markdown.fr";
+import "./../js/lib/bootstrap/markdown/i18n/bootstrap-markdown.fr";
+import "./../js/lib/pivotTable/i18n/pivot.fr";
+import "bootstrap-datepicker/js/locales/bootstrap-datepicker.fr";
