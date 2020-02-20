b1a931af03072443a7ebb8a56f56b24e1cb5d616
ALMIS\pgarcia <pablo.garcia@almis.com>
2020-02-20 12:44:43 +0100

    Fixed compilation

--- awe-framework/awe-client-angular/src/main/resources/webpack/locals-en.config.js
+++ awe-framework/awe-client-angular/src/main/resources/webpack/locals-en.config.js
@@ -1,5 +1,4 @@
 import "moment/locale/en-gb";
-import "numeral/locales/en-gb";
 import "./../js/lib/highcharts/i18n/highcharts.en";
 import "bootstrap-datepicker/js/locales/bootstrap-datepicker.en-GB";
 import "./../js/lib/pivotTable/i18n/pivot.en";
