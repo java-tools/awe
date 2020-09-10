a07faa94438473c98ca2bab191b2df6ba331c601
pvidal <pablo.vidal@almis.com>
2020-09-10 16:41:24 +0200

    Fix DeveloperTestSuite
    Signed-off-by: pvidal <pablo.vidal@almis.com>

--- awe-tests/awe-boot/src/test/java/com/almis/awe/test/unit/developer/DeveloperTestSuite.java
+++ awe-tests/awe-boot/src/test/java/com/almis/awe/test/unit/developer/DeveloperTestSuite.java
@@ -1,12 +1,11 @@
 package com.almis.awe.test.unit.developer;
 
-import com.almis.awe.developer.service.LiteralsService;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 
 @Suite.SuiteClasses({
-        LiteralsService.class,
+        LiteralsServiceTest.class,
 })
 
 @RunWith(Suite.class)
