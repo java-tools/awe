427862a175e94096ca873bfd71b6e8fe35c0bba7
pvidal <pablo.vidal@almis.com>
2020-09-10 16:09:54 +0200

    Add developer suite test
    Signed-off-by: pvidal <pablo.vidal@almis.com>

---
+++ awe-tests/awe-boot/src/test/java/com/almis/awe/test/unit/developer/DeveloperTestSuite.java
@@ -0,0 +1,15 @@
+package com.almis.awe.test.unit.developer;
+
+import com.almis.awe.developer.service.LiteralsService;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+
+@Suite.SuiteClasses({
+        LiteralsService.class,
+})
+
+@RunWith(Suite.class)
+public class DeveloperTestSuite {
+}
+
