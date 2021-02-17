dd2d42e88ec4e7494ec5433d4496f021c7129563
pvidal <pablo.vidal@almis.com>
2021-02-17 13:13:34 +0100

    Add wavefront metrics
    Signed-off-by: pvidal <pablo.vidal@almis.com>

--- awe-framework/awe-controller/src/main/java/com/almis/awe/controller/ActionController.java
+++ awe-framework/awe-controller/src/main/java/com/almis/awe/controller/ActionController.java
@@ -5,9 +5,11 @@ import com.almis.awe.model.entities.actions.ClientAction;
 import com.almis.awe.service.ActionService;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.sleuth.Tracer;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Objects;
 
 /**
  * Manage all incoming action requests
@@ -19,6 +21,7 @@ public class ActionController {
   // Autowired services
   private final ActionService actionService;
   private final AweRequest request;
+  private final Tracer tracer;
 
   /**
    * Autowired constructor
@@ -27,9 +30,10 @@ public class ActionController {
    * @param aweRequest    Awe request
    */
   @Autowired
-  public ActionController(ActionService actionService, AweRequest aweRequest) {
+  public ActionController(ActionService actionService, AweRequest aweRequest, Tracer tracer) {
     this.actionService = actionService;
     this.request = aweRequest;
+    this.tracer = tracer;
   }
 
   /**
@@ -45,6 +49,9 @@ public class ActionController {
 
     // Initialize parameters
     request.setParameterList(parameters);
+
+    // Span traces
+    this.addCustomSpan("actionId", actionId);
 
     // Launch action
     return actionService.launchAction(actionId);
@@ -67,7 +74,20 @@ public class ActionController {
     request.setTargetAction(targetId);
     request.setParameterList(parameters);
 
+    // Span traces
+    this.addCustomSpan("actionId", actionId);
+    this.addCustomSpan("targetId", targetId);
+
     // Launch action
     return actionService.launchAction(actionId);
+  }
+
+  /**
+   * Add custom span
+   * @param key Span name
+   * @param value Span value
+   */
+  private void addCustomSpan(String key, String value) {
+      Objects.requireNonNull(tracer.currentSpanCustomizer()).tag(key, value);
   }
 }
