bb1a3444251a77010ff38b1c26bcde492fdafc95
Pablo Javier García Mora <pablo.garcia@almis.com>
2021-02-24 11:19:08 +0000

    Resolve "Session parameters like site and database are not retrieved on login"

--- awe-framework/awe-controller/src/main/java/com/almis/awe/session/AweSessionDetails.java
+++ awe-framework/awe-controller/src/main/java/com/almis/awe/session/AweSessionDetails.java
@@ -13,6 +13,8 @@ import com.almis.awe.model.tracker.AweClientTracker;
 import com.almis.awe.model.tracker.AweConnectionTracker;
 import com.almis.awe.service.BroadcastService;
 import com.almis.awe.service.QueryService;
+import lombok.extern.log4j.Log4j2;
+import org.apache.logging.log4j.util.Strings;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
@@ -20,16 +22,15 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.util.Assert;
 
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
+import java.util.*;
+import java.util.stream.Stream;
 
 import static com.almis.awe.model.constant.AweConstants.*;
 
 /**
  * Session details
  */
+@Log4j2
 public class AweSessionDetails extends ServiceConfig {
 
   // Autowired services
@@ -79,18 +80,15 @@ public class AweSessionDetails extends ServiceConfig {
    */
   public void onLoginSuccess(Authentication authentication) {
     AweSession session = getSession().setAuthentication(authentication);
-    try {
-      // Store user in session
-      session.setParameter(SESSION_USER, session.getUser());
 
-      // Store user details
-      storeUserDetails();
+    // Store user in session
+    session.setParameter(SESSION_USER, session.getUser());
 
-      // Initialize session variables
-      initializeSessionVariables();
-    } catch (AWException exc) {
-      session.setParameter(SESSION_FAILURE, exc);
-    }
+    // Store user details
+    storeUserDetails();
+
+    // Initialize session variables
+    initializeSessionVariables();
   }
 
   /**
@@ -123,8 +121,8 @@ public class AweSessionDetails extends ServiceConfig {
     // Send logout broadcast to other connections
     connectionTracker.getUserConnectionsFromSession(user, getSession().getSessionId())
       .stream()
-      .filter(c -> !getRequest().getToken().equalsIgnoreCase(c))
-      .collect(Collectors.toSet())
+      .filter(Strings::isNotBlank)
+      .filter(c -> !c.equalsIgnoreCase(getRequest().getToken()))
       .forEach(c -> broadcastService.broadcastMessageToUID(c, new ClientAction("logout")));
 
     // Remove cometUID from user session
@@ -155,23 +153,30 @@ public class AweSessionDetails extends ServiceConfig {
 
   /**
    * Store user details
-   *
-   * @throws AWException User storage failed
    */
-  private void initializeSessionVariables() throws AWException {
-    AweSession session = getSession();
-    if (sessionParameters != null) {
-      for (String parameter : sessionParameters) {
-        if (!parameter.isEmpty()) {
-          String parameterQuery = getProperty("session." + parameter + ".query");
-          ServiceData queryOutput = queryService.launchQuery(parameterQuery, "1", "0");
-          DataList queryData = queryOutput.getDataList();
-          if (queryData != null && !queryData.getRows().isEmpty()) {
-            Map<String, CellData> row = queryData.getRows().get(0);
-            session.setParameter(parameter, row.get(AweConstants.JSON_VALUE_PARAMETER).getStringValue());
-          }
-        }
+  private void initializeSessionVariables() {
+    Optional.ofNullable(sessionParameters).orElse(Collections.emptyList())
+      .stream().filter(Strings::isNotBlank)
+      .forEach(this::evaluateSessionParameter);
+  }
+
+  /**
+   * Evaluate each session parameter
+   *
+   * @param parameter Parameter to evaluate
+   */
+  private void evaluateSessionParameter(String parameter) {
+    try {
+      String parameterQuery = getProperty("session." + parameter + ".query");
+      ServiceData queryOutput = queryService.launchQuery(parameterQuery, "1", "0");
+      DataList queryData = queryOutput.getDataList();
+      if (queryData != null && !queryData.getRows().isEmpty()) {
+        Map<String, CellData> row = queryData.getRows().get(0);
+        getSession().setParameter(parameter, row.get(AweConstants.JSON_VALUE_PARAMETER).getStringValue());
       }
+    } catch (Exception exc) {
+      log.error("There has been an error trying to retrieve the session parameter '{}'", parameter, exc);
+      getSession().setParameter(SESSION_FAILURE, exc);
     }
   }
 
@@ -184,32 +189,26 @@ public class AweSessionDetails extends ServiceConfig {
 
     Assert.notNull(userDetails, "User details must not be null. Check if the authentication provider saves user information in session");
 
-    // Get user data
-    String theme = defaultTheme;
-    String initialScreen = defaultInitialScreen;
-    String language = defaultLanguage;
-    String restriction = defaultRestriction;
-    String userTheme = userDetails.getUserTheme();
-    String profileTheme = userDetails.getProfileTheme();
-    String userRestriction = userDetails.getUserFileRestriction();
-    String profileRestriction = userDetails.getProfileFileRestriction();
-    String userInitialScreen = userDetails.getUserInitialScreen();
-    String profileInitialScreen = userDetails.getProfileInitialScreen();
+    // Specific language
+    String language = Optional.ofNullable(userDetails.getLanguage())
+      .filter(Strings::isNotBlank)
+      .orElse(defaultLanguage)
+      .substring(0, 2).toLowerCase();
 
-    // Specific language (from less to more specific)
-    language = userDetails.getLanguage().isEmpty() ? language : userDetails.getLanguage().substring(0, 2).toLowerCase();
+    // Specific restriction
+    String theme = Stream.of(userDetails.getUserTheme(), userDetails.getProfileTheme())
+      .filter(Strings::isNotBlank)
+      .findFirst().orElse(defaultTheme);
 
-    // Specific restriction (from less to more specific)
-    theme = profileTheme.isEmpty() ? theme : profileTheme;
-    theme = userTheme.isEmpty() ? theme : userTheme;
+    // Specific restriction
+    String restriction = Stream.of(userDetails.getUserFileRestriction(), userDetails.getProfileFileRestriction())
+      .filter(Strings::isNotBlank)
+      .findFirst().orElse(defaultRestriction);
 
-    // Specific restriction (from less to more specific)
-    restriction = profileRestriction.isEmpty() ? restriction : profileRestriction;
-    restriction = userRestriction.isEmpty() ? restriction : userRestriction;
-
-    // Specific initial screen (from less to more specific)
-    initialScreen = profileInitialScreen.isEmpty() ? initialScreen : profileInitialScreen;
-    initialScreen = userInitialScreen.isEmpty() ? initialScreen : userInitialScreen;
+    // Specific initial screen
+    String initialScreen = Stream.of(userDetails.getUserInitialScreen(), userDetails.getProfileInitialScreen())
+      .filter(Strings::isNotBlank)
+      .findFirst().orElse(defaultInitialScreen);
 
     // Store user data in session
     session.setParameter(SESSION_LAST_LOGIN, new Date());
